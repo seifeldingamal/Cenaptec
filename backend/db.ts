7731eda7d5fc39dd84e04c47e44672fbc4579a41
Seifeldin Abouelella <seifaldin.aboelella@gmail.com>
2022-09-12 18:45:09 +0200

    Revert "Changes to frontside"
    This reverts commit 2d068fd2efb75b3799d1488aa147318d6031a7ea.

--- backend/db.ts
+++ backend/db.ts
@@ -1,9 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
-import config from "./config";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import config from './config';
 
 // @ts-ignore
-const app = initializeApp(config);
+const app = initializeApp(config)
 const db = getFirestore(app);
 
 export default db;
--- frontend/src/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const api = "http://localhost:8000";
-const getText = () => fetch(`${api}/api/hello`, {
-    method: 'GET',
-})
-    .then(res => res.json());
-exports.default = getText;
